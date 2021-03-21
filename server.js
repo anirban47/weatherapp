347d11cb80059f6978ace9660fd5e45ff6dd124a
anirban <anirban552000@gmail.com>
2021-03-21 23:47:01 +0530

    Added first card on the weather report page

--- server.js
+++ server.js
@@ -11,23 +11,43 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", function (req, res) {
-  res.render("index.ejs");
+    res.render("index.ejs");
 });
 
-app.post("/", (req,res) => {
+app.post("/", (req, res) => {
     const query = req.body.query;
     const API_KEY = process.env.API_KEY;
-    const url = `https://api.openweathermap.org/data/2.5/forecast?q=${query}&appid=${API_KEY}`;
-    axios.get(url)
-      .then((response) => {
-          console.log("success");
-      })
-      .catch((error) => {
-         console.log(error);
-         res.render("error.ejs");
-      })
-})
+
+    const geocodingAPI = `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=1&appid=${API_KEY}`;
+
+    axios
+        .get(geocodingAPI)
+        .then((response) => {
+            // console.log(response);
+            return {
+                lat: response.data[0].lat,
+                lon: response.data[0].lon,
+                name: response.data[0].name,
+            };
+        })
+        .then((locationData) => {
+            const weatherAPI = `https://api.openweathermap.org/data/2.5/onecall?lat=${locationData.lat}&lon=${locationData.lon}&exclude=minutely&units=metric&appid=${API_KEY}`;
+            axios.get(weatherAPI).then((response) => {
+                console.log(response);
+                const weatherData = response.data;
+                const icon = weatherData.current.weather[0].icon;
+                const iconURL = `http://openweathermap.org/img/wn/${icon}@2x.png`
+                const date = weatherData.current.dt;
+                res.render("weatherReport.ejs", {
+                    locationName: locationData.name,
+                    iconURL: iconURL,
+                    date: (new Date(weatherData.current.dt)).toLocaleDateString(),
+                    temp: weatherData.current.temp,
+                });
+            });
+        });
+});
 
 app.listen(8000, () => {
-  console.log("Server is running on port 8000");
+    console.log("Server is running on port 8000");
 });
